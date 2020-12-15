f3d9a6d8e5280aac0110512aa038deb4f678310b
EP\hhng <hiep.nguyen@episerver.com>
2020-12-15 17:19:23 +0700

    Post list and create

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 const port = 4000;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
 app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
