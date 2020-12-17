8a2ad3ab9932e79ce96e520c6924f7edc3f9f887
EP\hhng <hiep.nguyen@episerver.com>
2020-12-17 10:31:23 +0700

    K8s: Update unique paths for ingress-controller

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -24,7 +24,7 @@ app.post('/posts/:id/comments', (req, res) => {
 
     commentsByPostId[req.params.id] = comments;
 
-    axios.post(`http://localhost:4005/events`, {
+    axios.post(`http://event-bus-srv:4005/events`, {
         type: 'CommentCreated',
         data: {
             id: commentId,
