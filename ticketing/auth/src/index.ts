340b921d0316ee273fba3b142893b02f6cc7d113
EP\hhng <hiep.nguyen@episerver.com>
2020-12-17 11:17:29 +0700

    apps: Add auth service skaffold file

---
+++ ticketing/auth/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+const port = 3000;
+app.use(json());
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
