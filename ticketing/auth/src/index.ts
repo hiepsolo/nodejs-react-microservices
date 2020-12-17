4fff22ce231c9da2468a5dd0d368cc992cda3de9
EP\hhng <hiep.nguyen@episerver.com>
2020-12-17 18:00:52 +0700

    ticket app: Add signin signup logic

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -10,8 +12,15 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 const port = 3000;
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -20,10 +29,28 @@ app.use(signoutRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
-})
+});
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-    console.log(`Listening on port: ${port}!!!`);
-});
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        console.log('Connected to DB');
+    } catch (error) {
+        console.error(error);
+    }
+
+    app.listen(port, () => {
+        console.log(`Listening on port: ${port}!!!!`);
+    });
+};
+
+start();
--- ticketing/auth/src/routes/singup.ts
+++ ticketing/auth/src/routes/singup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -12,20 +14,38 @@ router.post(
         body('password')
             .trim()
             .isLength({ min: 4, max: 20 })
-            .withMessage('Password must be between 4 and 20 characters'),
+            .withMessage('Password must be between 4 and 20 characters')
     ],
-    (req: Request, res: Response) => {
-        const errors = validationResult(req);
+    validateRequest,
+    async (req: Request, res: Response) => {
 
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
         }
 
-        console.log('Creating a user ...');
-        throw new DatabaseConnectionError();
+        const user = User.build({ email, password });
+        await user.save();
 
-        res.send({});
+        // Generate JWT
+        const userJwt = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+            },
+            process.env.JWT_KEY!
+        );
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt,
+        };
+
+        res.status(201).send(user);
     }
 );
 
 export { router as signupRouter };
+
