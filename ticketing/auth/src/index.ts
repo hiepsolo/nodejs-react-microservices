7a3702d3330cb9ad6af4443980c252f4a77615aa
EP\hhng <hiep.nguyen@episerver.com>
2020-12-17 14:52:34 +0700

    auth service: Error handling

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,10 +1,29 @@
 import express from 'express';
 import { json } from 'body-parser';
+import 'express-async-errors';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/singup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 const port = 3000;
 app.use(json());
 
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
+    console.log(`Listening on port: ${port}!!!`);
 });
