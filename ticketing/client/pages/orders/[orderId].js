cd1b1d31b5847051ae7296617181d9db2910c6e0
EP\hhng <hiep.nguyen@episerver.com>
2021-01-08 15:11:38 +0700

    client: done client

--- ticketing/client/pages/orders/[orderId].js
+++ ticketing/client/pages/orders/[orderId].js
@@ -1,23 +1,24 @@
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
 
-const OrderShow = ({order, currentUser}) => {
+const OrderShow = ({ order, currentUser }) => {
     const [timeLeft, setTimeLeft] = useState('');
-    const {doRequest, errors} = useRequest({
+    const { doRequest, errors } = useRequest({
         url: '/api/payments',
         method: 'post',
         body: {
-            orderId: order.id
+            orderId: order.id,
         },
-        onSuccess: (payment) => console.log(payment)
-    })
+        onSuccess: (payment) => Router.push('/orders'),
+    });
 
     useEffect(() => {
         const findTimeLeft = () => {
             const msLeft = new Date(order.expiresAt) - new Date();
             setTimeLeft(Math.round(msLeft / 1000));
-        }
+        };
         findTimeLeft();
         const timerId = setInterval(findTimeLeft, 1000);
 
@@ -33,22 +34,22 @@ const OrderShow = ({order, currentUser}) => {
     return (
         <div>
             Time left to pay: {timeLeft} seconds
-            <StripeCheckout 
-                token={({id}) => doRequest({token: id})}
+            <StripeCheckout
+                token={({ id }) => doRequest({ token: id })}
                 stripeKey="pk_test_XOMesR1gmxSjMU09MeaxQHat00JoQANiKs"
                 amount={order.ticket.price * 100}
                 email={currentUser.email}
             />
             {errors}
         </div>
-    )
+    );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
-    const {orderId} = context.query;
-    const {data} = await client.get(`/api/orders/${orderId}`);
+    const { orderId } = context.query;
+    const { data } = await client.get(`/api/orders/${orderId}`);
 
-    return {order: data};
-}
+    return { order: data };
+};
 
 export default OrderShow;
