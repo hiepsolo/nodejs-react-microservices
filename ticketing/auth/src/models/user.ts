83b82f6421bd056206acf48fe1a1e02436392b52
Hiep Nguyen <hoanghiep2004hy@gmail.com>
2021-01-10 20:15:46 +0700

    linode-k8s: udpate ingress nginx

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -9,16 +9,16 @@ interface UserAttrs {
 }
 
 // An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
 // that a User Document has
 interface UserDoc extends mongoose.Document {
     email: string;
     password: string;
-}
-
-// An interface that describes the properties
-// that a User Model has
-interface UserModel extends mongoose.Model<UserDoc> {
-    build(attrs: UserAttrs): UserDoc;
 }
 
 const userSchema = new mongoose.Schema({
